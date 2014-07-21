32539cf6edb33f60f1179a0f24ddffd15f8a6e6f
Matt Helm <matt@mtthlm.com>
2014-07-21 13:31:46 -0400

    Add dynamic element routes

--- TapPlugin.php
+++ TapPlugin.php
@@ -43,4 +43,38 @@ class TapPlugin extends BasePlugin
     {
         return 'https://mtthlm.com/';
     }
+
+    /**
+     * Register Site Routes
+     *
+     * @return array Site Routes
+     */
+    public function registerSiteRoutes()
+    {
+        $routes = array();
+
+        $request_type = craft()->request->getRequestType();
+
+        if ($request_type == 'GET') {
+            $routes['tap/(?P<element>\w+)'] = array('action' => 'tap/element/index');
+        }
+
+        if ($request_type == 'POST') {
+            $routes['tap/(?P<element>\w+)'] = array('action' => 'tap/element/store');
+        }
+
+        if ($request_type == 'GET') {
+            $routes['tap/(?P<element>\w+)/(?P<id>\d+)'] = array('action' => 'tap/element/show');
+        }
+
+        if (in_array($request_type, array('PUT', 'PATCH'))) {
+            $routes['tap/(?P<element>\w+)/(?P<id>\d+)'] = array('action' => 'tap/element/update');
+        }
+
+        if ($request_type == 'DELETE') {
+            $routes['tap/(?P<element>\w+)/(?P<id>\d+)'] = array('action' => 'tap/element/destroy');
+        }
+
+        return $routes;
+    }
 }
