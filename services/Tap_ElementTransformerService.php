b6dc2e3b5e2d08482a67e9c8a35ff76a5347202b
Matt Helm <matt@mtthlm.com>
2014-07-21 15:40:10 -0400

    Add DocBlocks

--- services/Tap_ElementTransformerService.php
+++ services/Tap_ElementTransformerService.php
@@ -4,11 +4,25 @@ namespace Craft;
 
 class Tap_ElementTransformerService extends BaseApplicationComponent
 {
+    /**
+     * Transform Collection
+     *
+     * @param array $collection Collection
+     *
+     * @return array Collection
+     */
     public function transformCollection(array $collection)
     {
         return array_map(array($this, 'transformElement'), $collection);
     }
 
+    /**
+     * Transform Element
+     *
+     * @param BaseElementModel $element Element
+     *
+     * @return array Element
+     */
     public function transformElement($element)
     {
         return $element;
