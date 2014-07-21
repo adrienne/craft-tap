7e142269a83aacb91cc399a4eaeb943be6b344c5
Matt Helm <matt@mtthlm.com>
2014-07-21 09:35:52 -0400

    Build Tap_ElementController Controller

---
+++ controllers/Tap_ElementController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Craft;
+
+class Tap_ElementController extends Tap_BaseController
+{
+    /**
+     * Allow Anonymous Access to Actions
+     *
+     * @var boolean
+     */
+    protected $allowAnonymous = true;
+}
