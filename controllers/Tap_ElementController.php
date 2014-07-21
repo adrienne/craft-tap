708b9fa2184ec561916eb2dcf3c009f5a0d1fa4f
Matt Helm <matt@mtthlm.com>
2014-07-21 13:31:37 -0400

    Create basic controller for Elements, and build out Index action method

--- controllers/Tap_ElementController.php
+++ controllers/Tap_ElementController.php
@@ -10,4 +10,53 @@ class Tap_ElementController extends Tap_BaseController
      * @var boolean
      */
     protected $allowAnonymous = true;
+
+    public function actionIndex(array $variables)
+    {
+        $element = $variables['element'];
+
+        $type = ucwords(strtolower($element));
+
+        try {
+            $criteria = craft()->elements->getCriteria($type);
+        } catch (Exception $exception) {
+            $this->respondBadRequest();
+        }
+
+        $results = $criteria->find();
+
+        return $this->respond(craft()->tap_elementTransformer->transformCollection($results));
+    }
+
+    public function actionStore(array $variables)
+    {
+        return $this->returnJson(array(
+            'action'    => 'store',
+            'variables' => $variables,
+        ));
+    }
+
+    public function actionShow(array $variables)
+    {
+        return $this->returnJson(array(
+            'action'    => 'show',
+            'variables' => $variables,
+        ));
+    }
+
+    public function actionUpdate(array $variables)
+    {
+        return $this->returnJson(array(
+            'action'    => 'update',
+            'variables' => $variables,
+        ));
+    }
+
+    public function actionDestroy(array $variables)
+    {
+        return $this->returnJson(array(
+            'action'    => 'destroy',
+            'variables' => $variables,
+        ));
+    }
 }
