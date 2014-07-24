9b0ef45f5338e5fa7f6e5107f70f4def1aff0c1b
Matt Helm <matt@mtthlm.com>
2014-07-24 15:54:40 -0400

    Create Routes Service

---
+++ services/Tap_RoutesService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Craft;
+
+class Tap_RoutesService extends BaseApplicationComponent
+{
+    /**
+     * Action Configs
+     *
+     * @var array
+     */
+    protected $actionConfigs = array(
+        'index'   => array('GET', '/'),
+        'store'   => array('POST', '/'),
+        'show'    => array('GET', '/(?P<id>\d+)'),
+        'update'  => array('PUT,PATCH', '/(?P<id>\d+)'),
+        'destroy' => array('DELETE', '/(?P<id>\d+)'),
+    );
+
+    /**
+     * Generate Elements Actions Routes
+     *
+     * @param array  $elements Elements
+     * @param string $prefix   Prefix
+     *
+     * @return array Routes
+     */
+    public function generateElementsActionsRoutes(array $elements, $prefix = null)
+    {
+        $routes = array();
+
+        $request_type = craft()->request->getRequestType();
+
+        foreach ($elements as $element => $actions) {
+            foreach ($actions as $action) {
+                if (array_key_exists($action, $this->actionConfigs) && in_array($request_type, explode(',', $this->actionConfigs[$action][0]))) {
+                    $routes[trim(trim($prefix, '/').'/(?P<element>'.$element.')/'.trim($this->actionConfigs[$action][1], '/'), '/')] = array('action' => 'tap/elementResource/'.$action);
+                }
+            }
+        }
+
+        return $routes;
+    }
+}
