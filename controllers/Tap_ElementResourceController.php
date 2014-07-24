2ff0d52df02caa54d29538b5892decc61e5317e6
Matt Helm <matt@mtthlm.com>
2014-07-24 13:30:16 -0400

    Close #9: Add support for manipulating the ElementCriteriaModel via Query String parameters

--- controllers/Tap_ElementResourceController.php
+++ controllers/Tap_ElementResourceController.php
@@ -9,6 +9,11 @@ class Tap_ElementResourceController extends Tap_ResourceController
         return str_replace(' ', '', ucwords(strtolower(str_replace('_', ' ', $element))));
     }
 
+    protected function getQuery()
+    {
+        return array_diff_key(craft()->request->getQuery(), array_flip(array('p')));
+    }
+
     public function index($element)
     {
         try {
@@ -17,9 +22,20 @@ class Tap_ElementResourceController extends Tap_ResourceController
             return $this->respondBadRequest();
         }
 
+        $query = $this->getQuery();
+
+        $criteria_attributes = $criteria->getAttributes();
+
+        foreach ($query as $name => $value) {
+            if (array_key_exists($name, $criteria_attributes)) {
+                $criteria->{$name} = $value;
+            }
+        }
+
         list($pagination, $elements) = TemplateHelper::paginateCriteria($criteria);
 
         return $this->respond(array(
+            'query'      => $query,
             'pagination' => $pagination,
             'elements'   => craft()->tap_elementTransformer->transformCollection($elements),
         ));
